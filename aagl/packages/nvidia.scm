b233a22a2f00053cc08c4c8fed0a4d3746d6ce08
Nikita Mitasov <me@ch4og.com>
2026-08-14 12:01:25 +0300

    aagl: Revamp nvidia variant packaging.
    * aagl/packages/nvidia.scm: Revamp nvidia variant packaging.
    * README.org: Update nvidia package documentation.

--- aagl/packages/nvidia.scm
+++ aagl/packages/nvidia.scm
@@ -3,108 +3,60 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (aagl packages nvidia)
+  #:use-module (ice-9 string-fun)
   #:use-module (nongnu packages nvidia)
+  #:use-module (aagl utils name)
+  #:use-module (aagl utils nvidia)
   #:use-module (guix packages)
   #:use-module (nonguix utils)
   #:use-module (aagl))
 
-(define-syntax define-nvidia-aagl
-  (syntax-rules ()
-    ((_ name alias-name builder driver)
-     (begin
-       (define-public name
-         (hidden-package (builder driver)))
-       (define-public alias-name
-         (package-with-alias
-          (string-append
-           (package-name (builder driver))
-           (cond ((eq? driver nvda-beta) "-beta")
-                 ((eq? driver nvda-new-feature) "-new-feature")
-                 (else "")))
-          (package
-            (inherit (builder driver))
-            (version (package-version driver)))))))))
+(define nvidia-versions
+  '(nvda-390
+    nvda-470
+    nvda-580
+    nvda-595
+    nvda-new-feature
+    nvda-beta))
 
-;;; an-anime-game
-(define-nvidia-aagl an-anime-game-launcher-nvidia-390 an-anime-game-launcher-nvidia-user-alias-390
-  an-anime-game-launcher-for nvda-390)
+(define nvidia-launchers
+  '(an-anime-game-launcher-for
+    honkers-launcher-for
+    the-honkers-railway-launcher-for
+    sleepy-launcher-for))
 
-(define-nvidia-aagl an-anime-game-launcher-nvidia-470 an-anime-game-launcher-nvidia-user-alias-470
-  an-anime-game-launcher-for nvda-470)
+(define (define-nvidia-variant! builder-name driver-name)
+  (let* ((builder (module-ref (current-module) builder-name))
+         (base-name (launcher-base-name builder-name))
+         (driver (module-ref (current-module) driver-name))
+         (suffix (nvidia-driver-suffix driver-name))
+         (variant-name
+          (string->symbol (string-append base-name "-nvidia-" suffix)))
+         (alias-name
+          (string->symbol
+           (string-append base-name "-nvidia-user-alias-" suffix)))
+         (variant (builder driver)))
+    (module-define! (current-module) variant-name (hidden-package variant))
+    (module-define!
+     (current-module) alias-name
+     (make-nvidia-alias
+      (nvidia-alias-name base-name suffix)
+      variant
+      driver))
+    (module-export! (current-module) (list variant-name alias-name))))
 
-(define-nvidia-aagl an-anime-game-launcher-nvidia-580 an-anime-game-launcher-nvidia-user-alias-580
-  an-anime-game-launcher-for nvda-580)
+(define (define-nvidia-default! builder-name)
+  (let* ((builder (module-ref (current-module) builder-name))
+         (name (string->symbol
+                (string-append (launcher-base-name builder-name) "-nvidia"))))
+    (module-define! (current-module) name (builder nvda))
+    (module-export! (current-module) (list name))))
 
-(define-nvidia-aagl an-anime-game-launcher-nvda-new-feature an-anime-game-launcher-nvidia-user-alias-590
-  an-anime-game-launcher-for nvda-new-feature)
-
-(define-nvidia-aagl an-anime-game-launcher-nvidia-595 an-anime-game-launcher-nvidia-user-alias-595
-  an-anime-game-launcher-for nvda-595)
-
-(define-nvidia-aagl an-anime-game-launcher-nvidia-beta an-anime-game-launcher-nvidia-user-alias-beta
-  an-anime-game-launcher-for nvda-beta)
-
-(define-public an-anime-game-launcher-nvidia (an-anime-game-launcher-for nvda))
-
-;;; honkers
-(define-nvidia-aagl honkers-launcher-nvidia-390 honkers-launcher-nvidia-user-alias-390
-  honkers-launcher-for nvda-390)
-
-(define-nvidia-aagl honkers-launcher-nvidia-470 honkers-launcher-nvidia-user-alias-470
-  honkers-launcher-for nvda-470)
-
-(define-nvidia-aagl honkers-launcher-nvidia-580 honkers-launcher-nvidia-user-alias-580
-  honkers-launcher-for nvda-580)
-
-(define-nvidia-aagl honkers-launcher-nvda-new-feature honkers-launcher-nvidia-user-alias-590
-  honkers-launcher-for nvda-new-feature)
-
-(define-nvidia-aagl honkers-launcher-nvidia-595 honkers-launcher-nvidia-user-alias-595
-  honkers-launcher-for nvda-595)
-
-(define-nvidia-aagl honkers-launcher-nvidia-beta honkers-launcher-nvidia-user-alias-beta
-  honkers-launcher-for nvda-beta)
-
-(define-public honkers-launcher-nvidia (honkers-launcher-for nvda))
-
-;;; the-honkers-railway
-(define-nvidia-aagl the-honkers-railway-launcher-nvidia-390 the-honkers-railway-launcher-nvidia-user-alias-390
-  the-honkers-railway-launcher-for nvda-390)
-
-(define-nvidia-aagl the-honkers-railway-launcher-nvidia-470 the-honkers-railway-launcher-nvidia-user-alias-470
-  the-honkers-railway-launcher-for nvda-470)
-
-(define-nvidia-aagl the-honkers-railway-launcher-nvidia-580 the-honkers-railway-launcher-nvidia-user-alias-580
-  the-honkers-railway-launcher-for nvda-580)
-
-(define-nvidia-aagl the-honkers-railway-launcher-nvda-new-feature the-honkers-railway-launcher-nvidia-user-alias-590
-  the-honkers-railway-launcher-for nvda-new-feature)
-
-(define-nvidia-aagl the-honkers-railway-launcher-nvidia-595 the-honkers-railway-launcher-nvidia-user-alias-595
-  the-honkers-railway-launcher-for nvda-595)
-
-(define-nvidia-aagl the-honkers-railway-launcher-nvidia-beta the-honkers-railway-launcher-nvidia-user-alias-beta
-  the-honkers-railway-launcher-for nvda-beta)
-
-(define-public the-honkers-railway-launcher-nvidia (the-honkers-railway-launcher-for nvda))
-
-;;; sleepy
-(define-nvidia-aagl sleepy-launcher-nvidia-390 sleepy-launcher-nvidia-user-alias-390
-  sleepy-launcher-for nvda-390)
-
-(define-nvidia-aagl sleepy-launcher-nvidia-470 sleepy-launcher-nvidia-user-alias-470
-  sleepy-launcher-for nvda-470)
-
-(define-nvidia-aagl sleepy-launcher-nvidia-580 sleepy-launcher-nvidia-user-alias-580
-  sleepy-launcher-for nvda-580)
-
-(define-nvidia-aagl sleepy-launcher-nvda-new-feature sleepy-launcher-nvidia-user-alias-590
-  sleepy-launcher-for nvda-new-feature)
-
-(define-nvidia-aagl sleepy-launcher-nvidia-595 sleepy-launcher-nvidia-user-alias-595
-  sleepy-launcher-for nvda-595)
-
-(define-nvidia-aagl sleepy-launcher-nvidia-beta sleepy-launcher-nvidia-user-alias-beta
-  sleepy-launcher-for nvda-beta)
-
-(define-public sleepy-launcher-nvidia (sleepy-launcher-for nvda))
+(map
+ (lambda (builder)
+   (map
+    (lambda (driver)
+      (define-nvidia-variant! builder driver))
+    nvidia-versions)
+   (define-nvidia-default! builder))
+ nvidia-launchers)
